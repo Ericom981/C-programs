a5d28970077463a36c0bbef28b43a1a9801a1a2a
Ericom981 <ericomnaj234@gmail.com>
2025-09-23 21:12:21 +0300

    Add files via upload

---
+++ my_details.c
@@ -0,0 +1,20 @@
+/*Name:Eric kamau ng'ang'a
+Reg. number:CT101/G/26471/25
+Description: program to enter my details 
+*/    
+#include<stdio.h>
+int main (){
+    int height;
+    printf ("enter your height:\n");    
+    scanf ("%d",& height);
+    printf ("your height is %d\n", height);
+    int bankbalance;
+    printf("enter your bankbalance:\n");
+    scanf("%d",& bankbalance);
+    printf("your bankbalance is %d\n",bankbalance);
+     int contact;
+    printf("enter your contact:\n");
+    scanf("%d",& contact);
+    printf ("your contact is %d\n", contact);
+    return 0;
+  }                
