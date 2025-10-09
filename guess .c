960902b34ae056e84f5e133343c8bda81fcd4df2
Ericom981 <ericomnaj234@gmail.com>
2025-10-09 20:47:53 +0300

    Add files via upload

---
+++ guess .c
@@ -0,0 +1,30 @@
+/*Name:Eric kamau ng'ang'a
+Reg. number:CT101/G/26471/25
+Description;count 100 to 50 in descending
+*/
+
+#include <stdio.h>
+
+int main() {
+    int secret = 7;   // fixed secret number
+    int guess;
+    int attempts = 0; // counter
+
+    while (1) {
+        printf("Enter your guess (1-20): ");
+        scanf("%d", &guess);
+        attempts++;   // increase count each time
+
+        if (guess > secret)
+            printf("Too high!\n");
+        else if (guess < secret)
+            printf("Too low!\n");
+        else {
+            printf("Congratulations!\n");
+            printf("You took %d attempts.\n", attempts);
+            break;
+        }
+    }
+
+    return 0;
+}
